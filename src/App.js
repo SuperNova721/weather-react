9ee7aeb84d5abc3375e33768d3551af2921be6a2
SuperNova721 <94159279+SuperNova721@users.noreply.github.com>
2022-03-18 02:19:00 -0700

    Added a footer with GitHub repository link in App.js and App.css

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import Search from "./Search";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <Search />
+    <div className="content">
+      <div className="App">
+        <h1>Weather App</h1>
+        <Search />
+        <footer>
+          This project was coded by Holly Hadley and is open-source on
+          <a
+            href="https://github.com/SuperNova721/weather-react.git"
+            target="_blank"
+            rel="noreferrer"
+            alt="GitHub"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
